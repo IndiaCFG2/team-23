d228b1554d575c402fa84a2f6e5ca5c4042ecdcb
ashishreddy <ashishpatlolla.reddy46@gmail.com>
2020-08-08 17:42:11 +0530

    added get functions for school,users,subjects

--- mutex-frontend/front-end/src/core/apiCore.js
+++ mutex-frontend/front-end/src/core/apiCore.js
@@ -1,11 +1,41 @@
 import { API } from "../config";
 
 export const getContents = sortBy => {
-    return fetch(`${API}/contents`, {
+    return fetch(`${API}/content`, {
         method: "GET"
     })
         .then(response => {
             return response.json();
         })
         .catch(err => console.log(err));
-       }
+       };
+
+export const getUsers = () => {
+    return fetch(`${API}/users`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+    };
+
+export const getSubjects = () => {
+    return fetch(`${API}/subject`, {
+         method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+    };
+
+export const getSchools = () => {
+    return fetch(`${API}/school`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+    };
